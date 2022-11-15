0ba8b0eae3fe04d46e81c10da5da9d0d85d346f4
mumulaptop2 <11902555+mumu176@user.noreply.gitee.com>
2022-11-15 11:21:46 +0800

    adjm

--- ex-ds/graph/adjm.c
+++ ex-ds/graph/adjm.c
@@ -3,21 +3,21 @@
 #include <stdbool.h>
 #define MAX_VERTEX_NUM 10
 #define INFINITY 32768
-typedef enum
+typedef enum//定义枚举图类型
 {
-    DG,
-    DN,
-    UDG,
-    UDN
+    DG,//有向图
+    DN,//有向网
+    UDG,//无向图
+    UDN//无向网
 } GraphKind;
 
-typedef char VertexData;
+typedef char VertexData;//图顶点数据类型是字符
 
-typedef struct arcnode
+typedef struct arcnode//定义边结构
 {
     int adj;
 } ArcNode;
-typedef struct adjmatrix
+typedef struct adjmatrix//定义邻接矩阵结构
 {
     VertexData vertex[MAX_VERTEX_NUM];
     ArcNode arcs[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
@@ -25,7 +25,7 @@ typedef struct adjmatrix
     GraphKind kind;
 } AdjMatrix;
 
-int LocateVertex(AdjMatrix *G, VertexData v)
+int LocateVertex(AdjMatrix *G, VertexData v)//通过元素寻找位置
 {
     int j = false, k;
     for (k = 0; k < G->vexnum; k++)
@@ -39,7 +39,7 @@ int LocateVertex(AdjMatrix *G, VertexData v)
     return j;
 }
 
-void CreatDN(AdjMatrix *G)
+void CreatDN(AdjMatrix *G)//创建有向网络
 {
     int i, j, k, weight;
     VertexData v1, v2;
@@ -51,13 +51,13 @@ void CreatDN(AdjMatrix *G)
     }
     for (i = 0; i < G->vexnum; i++)
     {
-        setbuf(stdin, NULL);
+        setbuf(stdin, NULL);//清空输入缓冲区
         G->vertex[i] = getchar();
     }
 
     for (k = 0; k < G->arcnum; k++)
     {
-        setbuf(stdin,NULL);
+        setbuf(stdin,NULL);//清空输入缓冲区
         scanf("%c %c %d", &v1, &v2, &weight);
         i = LocateVertex(G, v1);
         j = LocateVertex(G, v2);
@@ -66,7 +66,7 @@ void CreatDN(AdjMatrix *G)
     G->kind = DN;
 }
 
-void DispDN(AdjMatrix *G)
+void DispDN(AdjMatrix *G)//输出有向图的邻接矩阵
 {
     int i, j;
     printf("DN\t");
