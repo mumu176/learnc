58bae9843668319f7ee29b368d7afaf1f6c01eb6
mumulaptop1 <11902555+mumu176@user.noreply.gitee.com>
2022-11-18 17:20:18 +0800

    ex7

--- ex-c/ex-7/ex7-d5.c
+++ ex-c/ex-7/ex7-d5.c
@@ -1,27 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void guss(int n)
+void guess(int n)
 {
-    if(n%2==0&&n!=1&&n!=0)
+    if (n % 2 == 0 && n != 1)
     {
-        printf("%d/2=%d\n",n,n/2);
-        n=n/2;
-        guss(n);
+        printf("%d/2=%d\n", n, n / 2);
+        n = n / 2;
+        guess(n);
     }
-    else if(n%2==1&&n!=1&&n!=0)
+    else if (n % 2 == 1 && n != 1)
     {
-        printf("%d*3+1=%d\n",n,n*3+1);
-        n=n*3+1;
-        guss(n);
+        printf("%d*3+1=%d\n", n, n * 3 + 1);
+        n = n * 3 + 1;
+        guess(n);
     }
-    else if(n==1)
-    printf("End");
+    else if (n == 1)
+        printf("End\n");
 }
 
 int main()
 {
     int num;
-    scanf("%d",&num);
-    guss(num);
+    scanf("%d", &num);
+    if (num > 0)
+        guess(num);
     return 0;
 }
