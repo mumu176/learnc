ba84aa41143ea6c2d1f54c4eccd88b32074c2496
mumulaptop1 <11902555+mumu176@user.noreply.gitee.com>
2022-11-20 17:47:11 +0800

    work

--- ex-c/ex-7/ex7-d5.c
+++ ex-c/ex-7/ex7-d5.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-void guess(int n)
+void guess(long long int n)
 {
     if (n % 2 == 0 && n != 1)
     {
-        printf("%d/2=%d\n", n, n / 2);
+        printf("%lld/2=%lld\n", n, n / 2);
         n = n / 2;
         guess(n);
     }
     else if (n % 2 == 1 && n != 1)
     {
-        printf("%d*3+1=%d\n", n, n * 3 + 1);
+        printf("%lld*3+1=%lld\n", n, n * 3 + 1);
         n = n * 3 + 1;
         guess(n);
     }
     else if (n == 1)
-        printf("End\n");
+        printf("End");
 }
 
 int main()
 {
-    int num;
-    scanf("%d", &num);
+    long long int num;
+    scanf("%lld", &num);
     if (num > 0)
         guess(num);
     return 0;
---
+++ ex-java/test.java
@@ -0,0 +1,6 @@
+public class test{
+    public static void main(String[] args) {
+        char ch='你';
+        System.out.println(ch);
+    }
+}
