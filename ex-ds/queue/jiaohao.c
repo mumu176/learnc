c39adbf1eb1e3f72e2807d3baaeb52e6560642ed
mumulaptop2 <11902555+mumu176@user.noreply.gitee.com>
2022-12-06 17:54:30 +0800

    d

--- ex-ds/queue/jiaohao.c
+++ ex-ds/queue/jiaohao.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include<string.h>
-#define MAXSIZE 10
+#include <string.h>
+#define MAXSIZE 10//通过宏定义修改最大排队人数
 //循环队列定义
 typedef struct
 {
@@ -47,7 +47,7 @@ void dispqueue(SeqQueue q)
         for (i = q.front; i < q.rear; i++)
             printf("%c ", q.str[i]);
     }
-    else if(q.front!=q.rear)
+    else if (q.front != q.rear)
     {
         for (i = q.front; i < MAXSIZE; i++)
             printf("%c", q.str[i]);
@@ -56,44 +56,47 @@ void dispqueue(SeqQueue q)
     }
 }
 
-
-
-//开始
+//开始排号端，叫号端需要另一个线程
 int main()
 {
     SeqQueue q;
-    int i,num=1,front;
-    char ch,str[4];
+    int i, num = 1, front;
+    char ch, str[4];
     bool bo;
     q.front = 0; //初始化
     q.rear = 0;
-
-    while (num<100)
+    printf("-----排队排号系统v1-----\ninsert [in] or [out]\n");
+    while (scanf("%s", str) != EOF) //排号器输入ctrl-z停止
     {
-        scanf("%s",str);
-        if(strcmp("in",str)==0)
+
+        if (strcmp("in", str) == 0)
         {
-            inqueue(&q,'in');
-            if(q.front>=q.rear)
+            if (inqueue(&q, 'in') == true)
             {
-                front=q.front-q.rear;
+                if (q.front <= q.rear)
+                {
+                    front = q.rear - q.front - 1;
+                }
+                else
+                {
+                    front = q.rear + MAXSIZE - q.front - 1;
+                }
+                printf("your num is %d ,front %d\n", num, front);
+                num++;
+                printf("insert [in] or [out]\n");
             }
             else
             {
-                front=q.front+MAXSIZE-q.rear;
+                printf("wait someone out,press [in] one more time\n");
+                continue;
             }
-            printf("your num is %d ,front %d",num,front);
-            num++;
         }
-        else if(strcmp("out",str)==0)
+        else if (strcmp("out", str) == 0)
         {
             outqueue(&q);
+            printf("dequeue complete\n");
         }
-
-
     }
-    
-    
-    
+
     return 0;
 }
